9283cea35c0997473c9b0cf9a60671387587a8e9
mahmoud abou-elhamd <mahmoudabohassab00@gmail.com>
2023-08-23 07:41:04 +0300

    change app icon

--- app/src/main/java/com/mahmoud/android/cinematica/MainActivity.kt
+++ app/src/main/java/com/mahmoud/android/cinematica/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mahmoud.android.cinematica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.mahmoud.android.cinematica.databinding.ActivityMainBinding
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
